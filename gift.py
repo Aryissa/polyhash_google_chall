65afbecf69124b566bcfe37b2eb5d23562611bbe
egodec <egodec@smartadserver.com>
2022-11-18 16:50:13 +0100

    Ajout : class Gift

---
+++ gift.py
@@ -0,0 +1,7 @@
+class Gift:
+    def __init__(self, name, weight, x, y, score):
+        self.name = name
+        self.weight = weight
+        self.x = x
+        self.y = y
+        self.score = score
