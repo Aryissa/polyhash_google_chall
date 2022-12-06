55506df1f32a2397ea6f032b7f7f2a310c0b4924
Erwan Brunellière <ebrunelliere@softwaymedical.fr>
2022-12-06 15:13:21 +0100

    Ajout print des actions du père noel

--- Santa.py
+++ Santa.py
@@ -20,31 +20,43 @@ class Santa:
     def float(self):
         self.x += self.vx
         self.y += self.vy
+        self.output += '\nFloat 1'
 
     def accelerate(self, direction: str, value: int):
         speed = self.max_speed()
         if direction == 'vertical':
             self.vy += value
+            if value > 0:
+                self.output += f'\nAccRight {value}'
+            else:
+                self.output += f'\nAccLeft {value}'
             if self.vy > speed:
                 self.vy = speed
         else:
             self.vx += value
+            if value > 0:
+                self.output += f'\nAccUp {value}'
+            else:
+                self.output += f'\nAccDown {value}'
             if self.vx > speed:
                 self.vx = speed
         self.nb_carrots -= 1
         self.weight -= 1
+        self.float()
 
     def load_carrot(self, nb: int):
         if get_distance(self.x, self.y, 0, 0) > self.game.range:
             raise Exception('Distance trop grande pour le ramassage de carrotes')
         self.nb_carrots += nb
         self.weight += nb
+        self.output += f'\nLoadCarrots {nb}'
 
     def load_gift(self, gift: Gift):
         if get_distance(self.x, self.y, 0, 0) > self.game.range:
             raise Exception('Distance trop grande pour le ramassage de cadeaux')
         self.gifts.append(gift)
         self.weight += gift.weight
+        self.output += f'\nLoadGift {gift.name}'
 
     def deliver(self, gift: Gift):
         if get_distance(self.x, self.y, gift.x, gift.y) > self.game.range:
@@ -52,8 +64,13 @@ class Santa:
         self.score += gift.score
         self.weight -= gift.weight
         self.gifts.remove(gift)
+        self.output += f'\nDeliverGift {gift.name}'
 
     def max_speed(self):
         for k, v in self.game.acceleration_ranges:
             if k > self.weight:
                 return v
+
+    def print(self):
+        size = len(self.output.split('\n'))
+        return str(size) + self.output
