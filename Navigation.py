1e467adc3c772ac178f92477245244c1bc402c91
Erwan Brunellière <ebrunelliere@softwaymedical.fr>
2022-12-07 17:19:38 +0100

    Ajout fonction comptage plus grosse ligne

--- Navigation.py
+++ Navigation.py
@@ -1,10 +1,15 @@
 import Game
 import Gift
 import Santa
+import utils
 from utils import get_distance
 
 
 class Navigation:
+
+    def __init__(self, game, santa):
+        self.game = game
+        self.santa = santa
 
     def go_point(self, santa: Santa, x, y):
         while santa.x != x or santa.y != y:
@@ -29,3 +34,42 @@ class Navigation:
                     santa.accelerate("vertical", santa.max_speed())
             if (santa.vx != 0 or santa.vy != 0) and (santa.x != x or santa.y != y):
                 santa.float()
+
+    def go(self, x, y):
+        dx = abs(self.santa.x - x)
+        dy = abs(self.santa.y - y)
+
+    def gift_here(self, x, y):
+        for gift in self.game.gifts:
+            if gift.x == x and gift.y == y:
+                return True
+        return False
+
+    def line(self, x, y):
+        coordinates = [(gift.x, gift.y) for gift in self.game.gifts]
+        lines_x = dict()
+        lines_y = dict()
+        max_count = 0
+        max_vector = (0, 0)
+        # from x
+        for vector in utils.enumerate_vectors(4):
+            count = 0
+            for i in range(1000):
+                if (x + vector[0] * (i + 1), y + vector[1] * i) in coordinates:
+                    count += 1
+            lines_x[vector] = count
+            if count > max_count:
+                max_count = count
+                max_vector = vector
+        # from y
+        for vector in utils.enumerate_vectors(4):
+            count = 0
+            for i in range(1000):
+                if (x + vector[0] * i, y + vector[1] * (i + 1)) in coordinates:
+                    count += 1
+            lines_y[vector] = count
+            if count > max_count:
+                max_count = count
+                max_vector = vector
+
+        return max_vector, max_count
