d758f10d092529c296953afcdbdfecbaceea5588
egodec <egodec@smartadserver.com>
2022-12-07 09:09:05 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,4 +4,12 @@ import math
 def get_distance(x1, y1, x2, y2):
     dx = abs(x1 - x2)
     dy = abs(y1 - y2)
-    return math.sqrt(dx**2 + dy**2)
+    return math.sqrt(dx ** 2 + dy ** 2)
+
+
+def get_distance_x(x1, x2):
+    return abs(x1 - x2)
+
+
+def get_distance_y(y1, y2):
+    return abs(y1 - y2)
