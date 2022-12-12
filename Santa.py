b58a8ef28e3f91e8b25f5a2724ff03dae2d4a59c
Erwan Brunelliere <erwan.brunelliere@etudiant.univ-lr.fr>
2022-12-12 18:40:24 +0100

    Suppression d'un print + refonctionnement parseur originel

--- Santa.py
+++ Santa.py
@@ -97,7 +97,6 @@ class Santa:
 
         self.gifts.remove(gift)
         self.add_output(f'DeliverGift {gift.name}')
-        print(f'delivrage {gift.name} x:{gift.x} y:{gift.y}')
 
     def max_speed(self):
         for k, v in self.game.acceleration_ranges.items():
