49da51bb9ca260d7e8a2986eccf1371d78a88df9
Gabriel Comte <gabriel.comte@etu.univ-nantes.fr>
2023-01-26 18:44:18 +0100

    Finalisation commentaire Zone

--- Zone.py
+++ Zone.py
@@ -16,7 +16,6 @@ class Zone:
         self.gifts = gifts
         self.cluster=[]
         self.list_ratio=[]
-        #self.initial_gift = max(gifts, key=lambda gift: gift.ratio)
 
 
     def moyenne_points(self,map:Map,santa):
@@ -25,8 +24,10 @@ class Zone:
         distance des points pour chaque sous map.
         Retourne la moyenne de la moyenne de distance des points pour chaque sous-map
         """
-        list_scale=map.split_in_scale(santa.taille_map//10)
+        #On découpe la map en plusieur sous-map dans un tabelau
+        list_scale=map.split_in_scale(santa.taille_map//10) #le diviseur à été décidé comme cela car ça nous paraissé réaliste pour toute les map
         list_moyenne=[]
+        #pour chaque carré en calcul la moyenne de distance entre un point et tous les points qui sont dans la sous map pour tous les points et toutes les sou map
         for scale in list_scale:
             list_gift=[]
             for gift in self.gifts:
@@ -39,9 +40,11 @@ class Zone:
                         somme=somme+get_distance(gift_1.x, gift_1.y, gift_2.x, gift_2.y)
                     list_moyenne.append(somme/(len(list_gift)-1))
         somme_moyenne=0
+        #pour chaque moyenne total de sous map on fait la moyenne de toutes les sous map pour avoir une moyenne de distance de chaque cadeau dans la map
         for moyenne in list_moyenne:
             somme_moyenne=somme_moyenne+moyenne
 
+        #si on arrive pas a avoir de moyenne alors de base la moyenne de distance sera de 10 
         if(len(list_moyenne)==0):
             return 10
         return int(somme_moyenne/len(list_moyenne))+santa.taille_map//(santa.taille_map//diviseur(santa.taille_map))
@@ -52,18 +55,25 @@ class Zone:
         Création des clusters
         distance_moyenne: la moyenne de distance des points sur la map (obtenue par la méthode: moyenne_points)
         """
+        #Un cluster se créer par rapport à la distance moyenne des cadeaux dans la map
         list_cluster=[]
+        #pour chaque cadeaux
         for gift_1 in self.gifts:
             list_courante=[]
             for list_c in list_cluster:
+                #s'il est déja dans un cluster alors on récpère le cluster pour retravailler dessus car un cadeaux ne peux pas se trouver dans 2 clusters différents
                 if gift_1 in list_c:
                     list_courante=list_c
                     list_cluster.remove(list_c)
                     break;
+            #pour tous les autres cadeau
             for gift_2 in self.gifts:
+                #s'il n'est pas déjà dans le cluster car nous ne souhaitons pas avoir de doublons
                 if(gift_2 not in list_courante):
                     if (gift_2.x in range(gift_1.x-distance_moyenne,gift_1.x+distance_moyenne) and gift_2.y in range(gift_1.y-distance_moyenne,gift_1.y+distance_moyenne)):
+                        #on met le cadeau dans le cluster
                         list_courante.append(gift_2)
+            #si la liste courante est vide c'est à dire qu'aucun cluster ne contient le cadeau
             if(list_courante==[]):
                 list_courante.append(gift_1)
             list_cluster.append(list_courante)
